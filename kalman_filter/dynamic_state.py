74d0cb8ccfffb91b74736bbf2c39d8abc14bf577
Albertus Kelvin <albertus.kelvin@gdplabs.id>
2020-09-25 19:02:25 +0700

    add predicted estimate error

--- kalman_filter/dynamic_state.py
+++ kalman_filter/dynamic_state.py
@@ -4,26 +4,34 @@ import numpy as np
 from kalman_filter.constants.dynamic_state import DynamicStateMultipleTrueValues1D
 
 def calculate_initial_process_covariance():
-	cov = DynamicStateMultipleTrueValues1D.PROCESS_ERROR_POSITION * DynamicStateMultipleTrueValues1D.PROCESS_ERROR_VELOCITY
+	cov = DynamicStateMultipleTrueValues1D.ESTIMATE_ERROR_POSITION * DynamicStateMultipleTrueValues1D.ESTIMATE_ERROR_VELOCITY
 
+	# replace cov with 0
 	return np.array([
-		[math.pow(DynamicStateMultipleTrueValues1D.PROCESS_ERROR_POSITION, 2), cov], 
-		[cov, math.pow(DynamicStateMultipleTrueValues1D.PROCESS_ERROR_VELOCITY, 2)]
+		[math.pow(DynamicStateMultipleTrueValues1D.ESTIMATE_ERROR_POSITION, 2), 0.0], 
+		[0.0, math.pow(DynamicStateMultipleTrueValues1D.ESTIMATE_ERROR_VELOCITY, 2)]
 	])
 
 
+def calculate_predicted_process_covariance(previous_process_covariance):
+	return (
+		DynamicStateMultipleTrueValues1D.STATE_MULTIPLIER_MATRIX.dot(previous_process_covariance)
+	).dot(DynamicStateMultipleTrueValues1D.STATE_MULTIPLIER_MATRIX.transpose()) \
+	+ DynamicStateMultipleTrueValues1D.PREDICTED_ESTIMATE_ERROR_MATRIX
+
+
 def calculate_predicted_state(previous_state):
-	return DynamicStateMultipleTrueValues1D.STATE_MATRIX_MULTIPLIER.dot(previous_state) \
-	+ DynamicStateMultipleTrueValues1D.CONTROL_VARIABLE_MATRIX_MULTIPLIER.dot(DynamicStateMultipleTrueValues1D.CONTROL_VARIABLE_MATRIX) \
+	return DynamicStateMultipleTrueValues1D.STATE_MULTIPLIER_MATRIX.dot(previous_state) \
+	+ DynamicStateMultipleTrueValues1D.CONTROL_VARIABLE_MULTIPLIER_MATRIX.dot(DynamicStateMultipleTrueValues1D.CONTROL_VARIABLE_MATRIX) \
 	+ DynamicStateMultipleTrueValues1D.STATE_PREDICTION_ERROR_MATRIX
 
 
 def estimate_multiple_true_value_1d():
 	measurements = readFromFile(DynamicStateMultipleTrueValues1D.MEASUREMENT_DATA_FILE)
 
-	initial_process_covariance = calculate_initial_process_covariance()
+	previous_process_covariance = calculate_initial_process_covariance()
 
- 	for measurement in measurements:
+	for measurement in measurements:
 		position, velocity = measurement.split(",")
 		position, velocity = float(position), float(velocity)
 
@@ -33,7 +41,9 @@ def estimate_multiple_true_value_1d():
 		# calculate predicted state
 		predicted_state = calculate_predicted_state(previous_state)
 
-
+		# predicted process covariance
+		predicted_process_covariance = calculate_predicted_process_covariance(previous_process_covariance)
+		print(predicted_process_covariance)
 
 def readFromFile(path: str) -> [float]:
 	with open(path, "r") as f:
